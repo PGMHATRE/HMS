63468cebca3a504100499760b6fa702e7a2967a1
SaniyaPathan25 <130686942+SaniyaPathan25@users.noreply.github.com>
2023-04-19 16:23:42 +0530

    Create leave_details.php

---
+++ admin/leave_details.php
@@ -0,0 +1,213 @@
+<?php
+    session_start();
+    include('../includes/dbconn.php');
+    include('../includes/check-login.php');
+    check_login();
+?>
+
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="../assets/images/favicon.png">
+    <title>Girls's Hostel Management System</title>
+    <!-- Custom CSS -->
+    <link href="../assets/extra-libs/c3/c3.min.css" rel="stylesheet">
+    <link href="../assets/libs/chartist/dist/chartist.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="../dist/css/style.min.css" rel="stylesheet">
+
+    
+</head>
+
+<body>
+    <!-- ============================================================== -->
+    <!-- Preloader - style you can find in spinners.css -->
+    <!-- ============================================================== -->
+    <div class="preloader">
+        <div class="lds-ripple">
+            <div class="lds-pos"></div>
+            <div class="lds-pos"></div>
+        </div>
+    </div>
+    <!-- ============================================================== -->
+    <!-- Main wrapper - style you can find in pages.scss -->
+    <!-- ============================================================== -->
+    <div id="main-wrapper" data-theme="light" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+        data-sidebar-position="fixed" data-header-position="fixed" data-boxed-layout="full">
+        <!-- ============================================================== -->
+        <!-- Topbar header - style you can find in pages.scss -->
+        <!-- ============================================================== -->
+        <header class="topbar" data-navbarbg="skin6">
+            <?php include 'includes/navigation.php'?>
+        </header>
+        <!-- ============================================================== -->
+        <!-- End Topbar header -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Left Sidebar - style you can find in sidebar.scss  -->
+        <!-- ============================================================== -->
+        <aside class="left-sidebar" data-sidebarbg="skin6">
+            <!-- Sidebar scroll-->
+            <div class="scroll-sidebar" data-sidebarbg="skin6">
+                <?php include 'includes/sidebar.php'?>
+            </div>
+            <!-- End Sidebar scroll-->
+        </aside>
+        <!-- ============================================================== -->
+        <!-- End Left Sidebar - style you can find in sidebar.scss  -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Page wrapper  -->
+        <!-- ============================================================== -->
+        <div class="page-wrapper">
+            <!-- ============================================================== -->
+            <!-- Bread crumb and right sidebar toggle -->
+            <!-- ============================================================== -->
+            <div class="page-breadcrumb">
+                <div class="row">
+
+
+                    <div class="col-7 align-self-center">
+                    <h4 class="page-title text-truncate text-dark font-weight-medium mb-1">Full Details</h4>
+                        <div class="d-flex align-items-center">
+                            <!-- <nav aria-label="breadcrumb">
+                                
+                            </nav> -->
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Bread crumb and right sidebar toggle -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+            <!-- Container fluid  -->
+            <!-- ============================================================== -->
+            <div class="container-fluid">
+
+                <!--Table Column -->
+                
+                <div class="card">
+                 
+                 <div class="card-body">
+                 
+                    <div class="row">
+                    
+                    <div class="table-responsive">
+                                  <table id="zctb" class="table table-striped table-bordered no-wrap">
+
+                                      <tbody>
+
+                                      <?php	
+                                      
+                                      $id=$_GET['id'];
+                                        $ret="SELECT * from leaveappication where sr_no=?";
+                                        $stmt= $mysqli->prepare($ret) ;
+                                    $stmt->bind_param('i',$id);
+                                    $stmt->execute() ;//ok
+                                    $res=$stmt->get_result();
+                                    //$cnt=1;
+                                    while($row=$res->fetch_object())
+                                    {
+                                              ?>
+                                        
+                                          <tr>
+                                          <td><b>Room no :</b></td>
+                                          <td><?php echo $row->room_no;?></td>
+                                          <td><b>Leaving Date :</b></td>
+                                          <td><?php echo $row->leaving_date;?></td>
+                                          <td><b>Back date</b></td>
+                                          <td><?php echo $row->back_date;?></td>
+                                          </tr>
+
+
+                                          <tr>
+                                          <td><b>Going Address:</b></td>
+                                          <td><?php echo $row->goingaddress;?></td>
+                                          <td><b>Going City:</b></td>
+                                          <td><?php echo $row->goingcity;?></td>
+                                          <td><b>Going Pin Code:</b></td>
+                                          <td><?php echo $row->goingpincode;?></td>
+                                          <td><b>Parent's mail:</b></td>
+                                          <td><?php echo $row->pemail;?></td>
+                                          </tr>
+
+                                          <tr>
+                                          <td><b>Parent's Approval Letter:</b></td>
+                                          <td><?php echo $row->parentApplication;?></td>
+                                          <tr>
+
+
+                                          <?php } ?>
+
+                                      </tbody>
+                                  </table>
+                                  <a class="btn btn-success" href="send_mail.php" role="button">Accept</a>
+                                  <a class="btn btn-danger" href="reject_reason.php" role="button">Reject</a>
+                                 
+                              </div>
+                    
+                    
+                    </div>
+                 
+                 
+                 </div>
+               
+               
+               </div>
+
+              <!-- Table column end -->
+
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Container fluid  -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+            <!-- footer -->
+            <!-- ============================================================== -->
+            <?php include '../includes/footer.php' ?>
+            <!-- ============================================================== -->
+            <!-- End footer -->
+            <!-- ============================================================== -->
+        </div>
+        <!-- ============================================================== -->
+        <!-- End Page wrapper  -->
+        <!-- ============================================================== -->
+    </div>
+    <!-- ============================================================== -->
+    <!-- End Wrapper -->
+    <!-- ============================================================== -->
+    <!-- End Wrapper -->
+    <!-- ============================================================== -->
+    <!-- All Jquery -->
+    <!-- ============================================================== -->
+    <script src="../assets/libs/jquery/dist/jquery.min.js"></script>
+    <script src="../assets/libs/popper.js/dist/umd/popper.min.js"></script>
+    <script src="../assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- apps -->
+    <!-- apps -->
+    <script src="../dist/js/app-style-switcher.js"></script>
+    <script src="../dist/js/feather.min.js"></script>
+    <script src="../assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
+    <script src="../dist/js/sidebarmenu.js"></script>
+    <!--Custom JavaScript -->
+    <script src="../dist/js/custom.min.js"></script>
+    <!--This page JavaScript -->
+    <script src="../assets/extra-libs/c3/d3.min.js"></script>
+    <script src="../assets/extra-libs/c3/c3.min.js"></script>
+    <script src="../assets/libs/chartist/dist/chartist.min.js"></script>
+    <script src="../assets/libs/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js"></script>
+    <script src="../dist/js/pages/dashboards/dashboard1.min.js"></script>
+
+</body>
+
+</html>
