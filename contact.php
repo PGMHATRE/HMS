05977259d9909c305b40010f2ec20c4228f77f9c
NamanAcharya-Dev <namanacharya245@gmail.com>
2023-05-05 09:49:57 +0530

    Contact Page Mail added

--- contact.php
+++ contact.php
@@ -1,5 +1,6 @@
 <?php 
     include('include/header.php');
+    include('include/sendmail.php');
 ?>
     <section data-bs-version="5.1" class="contacts2 cid-tDjpr5VdMc" id="contacts2-13">
         <div class="container">
@@ -51,12 +52,18 @@
             </div>
             <div class="row justify-content-center mt-4">
                 <div class="col-lg-8 mx-auto mbr-form" data-form-type="formoid">
-                    <form action="" method="POST" class="mbr-form form-with-styler" data-form-title="Form Name"><input type="hidden" name="email" data-form-email="true" value="cOgf2dmothlyKiBou4yQoonOgDJHnn20uxUOgo2c6LfhBjUgYGdRRRa+4JXOVQ/H76O8p5BrvvKVCzMDRb9Dkd1lryvXHw9etK1vOxKg6f4mh4IBn4PeSeuqHwhaQqBU">
+                    <form action="" method="POST" class="mbr-form form-with-styler" data-form-title="Form Name">
                         <div class="row">
-                            <div hidden="hidden" data-form-alert="" class="alert alert-success col-12">Thanks For Filling Out We Will Contact You Soon!!</div>
-                            <div hidden="hidden" data-form-alert-danger="" class="alert alert-danger col-12">
-                                Oops...! some problem!
-                            </div>
+                            <?php if(isset($_SESSION['response_msg']) && isset($_SESSION['response_msg_type'])){ ?>
+                                <div class="col-12">
+                                    <div class="alert alert-<?php echo $_SESSION['response_msg_type']; ?>">
+                                        <?php echo $_SESSION['response_msg']; ?>
+                                    </div>
+                                </div>
+                            <?php 
+                                unset($_SESSION['response_msg']);
+                                unset($_SESSION['response_msg_type']);
+                            } ?>
                         </div>
                         <div class="dragArea row">
                             <div class="col-md col-sm-12 form-group mb-3" data-for="name">
@@ -65,14 +72,14 @@
                             <div class="col-md col-sm-12 form-group mb-3" data-for="email">
                                 <input type="email" name="email" placeholder="E-mail" data-form-field="email" class="form-control" value="" id="email-form5-14">
                             </div>
-                            <div class="col-12 form-group mb-3" data-for="url">
-                                <input type="url" name="url" placeholder="Subject" data-form-field="url" class="form-control" value="" id="url-form5-14">
+                            <div class="col-12 form-group mb-3" data-for="subject">
+                                <input type="text" name="subject" placeholder="Subject" data-form-field="subject" class="form-control" value="" id="url-form5-14">
                             </div>
                             <div class="col-12 form-group mb-3" data-for="textarea">
-                                <textarea name="textarea" placeholder="Message" data-form-field="textarea" class="form-control" id="textarea-form5-14"></textarea>
+                                <textarea name="message" placeholder="Message" data-form-field="textarea" class="form-control" id="textarea-form5-14"></textarea>
                             </div>
                             <div class="col-lg-12 col-md-12 col-sm-12 align-center mbr-section-btn">
-                                <button type="submit" class="btn btn-primary display-4">Send message</button>
+                                <button type="submit" name="send" class="btn btn-primary display-4">Send message</button>
                             </div>
                         </div>
                     </form>
